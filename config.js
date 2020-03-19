dc883eeb8d1023ac8c095667c35a2f0c7520996a
Songwutt <5735512158@psu.ac.th>
2020-03-19 13:34:37 +0700

    React

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAf0azATqXEcXhcxOkKZWzy3e40f5oPupI",
+    authDomain: "todo-240311-1.firebaseapp.com",
+    databaseURL: "https://todo-240311-1.firebaseio.com",
+    projectId: "todo-240311-1",
+    storageBucket: "todo-240311-1.appspot.com",
+    messagingSenderId: "809574581737",
+    appId: "1:809574581737:web:967a2784eb5245f46c3778",
+    measurementId: "G-NXN5Q0KFR0"
+  };
+
+  export default config
